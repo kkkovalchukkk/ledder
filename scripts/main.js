7df229eb8ffd458ae7fc46669e956f9a137f461d
kkkovalchukkk <student.kovalchuk@yandex.ru>
2023-09-13 16:51:24 +0300

    init

---
+++ scripts/main.js
@@ -0,0 +1,355 @@
+$(document).ready(function () {
+  Fancybox.bind('[data-fancybox]', {
+    // Your custom options
+  })
+
+  const formBoxLabel = document.querySelectorAll('.form__box-label')
+
+  if (formBoxLabel) {
+    formBoxLabel.forEach((item) => {
+      item.addEventListener('click', function () {
+        formBoxLabel.forEach((item) => {
+          item.classList.remove('form__box-label--checked')
+        })
+        this.classList.add('form__box-label--checked')
+      })
+    })
+  }
+
+  const gallerySlide = document.querySelectorAll('.interview__box')
+
+  gallerySlide?.forEach((elem) => {
+    const galleryImg = elem.querySelector('.interview__img')
+    const galleryHidden = elem.querySelector('.interview__hidden')?.firstElementChild
+    galleryImg.addEventListener('click', () => {
+      galleryHidden.click()
+    })
+  })
+
+  const modalBoxLabel = document.querySelectorAll('.modal__box-label')
+  modalBoxLabel.forEach((label) => {
+    label.addEventListener('click', function () {
+      modalBoxLabel.forEach((label) => {
+        if (label.firstElementChild?.checked) {
+          label.classList.remove('modal__box-label--checked')
+        }
+      })
+      this.classList.add('modal__box-label--checked')
+    })
+  })
+
+  const body = document.querySelector('body')
+  const burger = document.querySelector('.burger')
+  const close = document.querySelector('.menu__close')
+  const menu = document.querySelector('.menu')
+  const menuItem = document.querySelectorAll('.menu__item')
+
+  if (burger) {
+    $(document).on('click', function (e) {
+      if (!$(e.target).closest('.active').length) {
+        burger.classList.remove('active')
+        menu.classList.remove('active')
+        body.classList.remove('no-scroll')
+      }
+      e.stopPropagation()
+    })
+
+    menuItem.forEach((item) => {
+      item.addEventListener('click', () => {
+        burger.classList.toggle('active')
+        menu.classList.remove('active')
+        body.classList.remove('no-scroll')
+      })
+    })
+
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active')
+      menu.classList.toggle('active')
+      body.classList.toggle('no-scroll')
+    })
+
+    close.addEventListener('click', () => {
+      burger.classList.toggle('active')
+      menu.classList.toggle('active')
+      body.classList.toggle('no-scroll')
+    })
+  }
+
+  if (document.querySelector('.form-input-tel')) {
+    $('.form-input-tel').mask('+7 (999) 999-99-99', {
+      autoclear: false,
+    })
+  }
+
+  const lang = document.querySelector('.header__lang')
+  lang.addEventListener('click', () => {
+    if (lang.classList.contains('header__lang--active')) {
+      lang.classList.remove('header__lang--active')
+    } else {
+      lang.classList.add('header__lang--active')
+    }
+  })
+
+  const langNow = document.querySelector('.header__lang-now')
+  const langBtn = document.querySelector('.header__lang-btn')
+
+  langBtn.addEventListener('click', () => {
+    const text = langNow.textContent
+    langNow.textContent = langBtn.textContent
+    langBtn.textContent = text
+  })
+
+  const whyItemNum = document.querySelectorAll('.why__item-num')
+
+  function elemLength(elem) {
+    elem.forEach((span, index) => {
+      index >= 9 ? (span.textContent = `${index + 1}`) : (span.textContent = `0${index + 1}`)
+    })
+  }
+  elemLength(whyItemNum)
+
+  $('.questions__item').click(function () {
+    $(this).toggleClass('questions__item--active').children().eq(1).slideToggle()
+  })
+
+  $('.model__item').click(function () {
+    $(this).toggleClass('model__item--active').children().eq(1).slideToggle()
+    $('.model__item')
+      .not(this)
+      .removeClass('model__item--active')
+      .find('.model__item-hidden')
+      .slideUp()
+  })
+
+  $('.services__item').click(function () {
+    $(this).toggleClass('services__item--active').children().eq(1).slideToggle()
+    $('.services__item')
+      .not(this)
+      .removeClass('services__item--active')
+      .find('.services__item-hidden')
+      .slideUp()
+  })
+
+  if (innerWidth < 480) {
+    $('.why__item').click(function () {
+      $(this).toggleClass('why__item--active').children().eq(1).slideToggle()
+    })
+  }
+
+  var targetMap = document.querySelectorAll('[data-target]'),
+    map = document.querySelectorAll('.tab__info')
+
+  targetMap?.forEach((elem) => {
+    elem.addEventListener('click', function (e) {
+      e.preventDefault()
+      var target = this.getAttribute('data-target')
+      map.forEach((elem) => {
+        elem.classList.remove('tab__info--opacity', 'tab__info--active')
+      })
+      targetMap.forEach((elem) => {
+        elem.classList.remove('tab__target--active')
+      })
+      this.classList.add('tab__target--active')
+      var cat = document.querySelector('[data-info="' + target + '"]')
+      cat.classList.add('tab__info--active')
+      setTimeout(() => {
+        cat.classList.add('tab__info--opacity')
+      }, 400)
+    })
+  })
+
+  if (document.querySelector('.projects__swiper')) {
+    const projectsSwiper = new Swiper('.projects__swiper', {
+      navigation: {
+        prevEl: '.projects__arrow-prev',
+        nextEl: '.projects__arrow-next',
+      },
+      autoplay: {
+        delay: 5000,
+      },
+      breakpoints: {
+        1025: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        767: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+          grid: {
+            rows: 2,
+          },
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+          grid: {
+            rows: 2,
+          },
+        },
+      },
+    })
+  }
+
+  if (document.querySelector('.services__swiper')) {
+    const servicesSwiper = new Swiper('.services__swiper', {
+      navigation: {
+        prevEl: '.services__arrow-prev',
+        nextEl: '.services__arrow-next',
+      },
+      autoplay: {
+        delay: 5000,
+      },
+      slidesPerView: 1,
+      spaceBetween: 40,
+    })
+  }
+
+  if (document.querySelector('.article__swiper')) {
+    const articleSwiper = new Swiper('.article__swiper', {
+      navigation: {
+        prevEl: '.article__arrow-prev',
+        nextEl: '.article__arrow-next',
+      },
+      loop: true,
+      autoplay: {
+        delay: 5000,
+      },
+      slidesPerView: 1,
+      spaceBetween: 40,
+      breakpoints: {
+        1025: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 2.2,
+          spaceBetween: 20,
+        },
+        480: {
+          slidesPerView: 1.2,
+          spaceBetween: 20,
+        },
+        360: {
+          slidesPerView: 1.2,
+          spaceBetween: 20,
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+      },
+    })
+  }
+
+  if (document.querySelector('.article__swiperTwo')) {
+    const articleSwiperTwo = new Swiper('.article__swiperTwo', {
+      navigation: {
+        prevEl: '.article__arrow--prevTwo',
+        nextEl: '.article__arrow--nextTwo',
+      },
+      autoplay: {
+        delay: 5000,
+      },
+      slidesPerView: 2.4,
+      spaceBetween: 40,
+      breakpoints: {
+        1025: {
+          slidesPerView: 2.25,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 1.6,
+          spaceBetween: 20,
+        },
+        480: {
+          slidesPerView: 0.98,
+          spaceBetween: 20,
+        },
+        360: {
+          slidesPerView: 1.35,
+          spaceBetween: 20,
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+      },
+    })
+  }
+
+  if (document.querySelector('.article__swiperThree')) {
+    const articleSwiperThree = new Swiper('.article__swiperThree', {
+      navigation: {
+        prevEl: '.article__arrow--prevThree',
+        nextEl: '.article__arrow--nextThree',
+      },
+      autoplay: {
+        delay: 5000,
+      },
+      slidesPerView: 1.9,
+      spaceBetween: 40,
+      breakpoints: {
+        1025: {
+          slidesPerView: 1.7,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 1.2,
+          spaceBetween: 20,
+        },
+        480: {
+          slidesPerView: 1.1,
+          spaceBetween: 20,
+        },
+        360: {
+          slidesPerView: 1.1,
+          spaceBetween: 20,
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+      },
+    })
+  }
+
+  if (document.querySelector('.article__swiperWidth')) {
+    const articleSwiperTwo = new Swiper('.article__swiperWidth', {
+      navigation: {
+        prevEl: '.article__arrow-prevWidth',
+        nextEl: '.article__arrow-nextWidth',
+      },
+      autoplay: {
+        delay: 5000,
+      },
+      slidesPerView: 1,
+      spaceBetween: 40,
+      breakpoints: {
+        1025: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        360: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+      },
+    })
+  }
+})
---
+++ scripts/valid.js
@@ -0,0 +1,179 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  const modalFormEl = document.querySelector('#modal-form');
+  const middleFormEl = document.querySelector('#middle-form');
+  const footerFormEl = document.querySelector('.footer__form');
+  const modalNameInputEl = modalFormEl.querySelector('#modal-name-input');
+  const modalEmailInputEl = modalFormEl.querySelector('#modal-email-input');
+  const modalTelephoneInputEl = modalFormEl.querySelector('#modal-tel-input');
+  const middleNameInputEl = middleFormEl?.querySelector('#modal-name-input');
+  const middleEmailInputEl = middleFormEl?.querySelector('#modal-email-input');
+  const middleTelephoneInputEl = middleFormEl?.querySelector('#modal-tel-input');
+
+  const checkInputValidity = (input) => input.value;
+
+  const submitFooterForm = (e) => {
+    e.preventDefault();
+
+    const nameInput = e.target.footer_form_name;
+    const telInput = e.target.footer_form_tel;
+    const emailInput = e.target.footer_form_email;
+    [nameInput, telInput, emailInput].forEach((input) => {
+      if (!checkInputValidity(input)) {
+        input.classList.add('input--invalid');
+        return;
+      } else {
+        input.classList.remove('input--invalid');
+      }
+    });
+
+    if (
+      nameInput.value.length < 2 ||
+      nameInput.value.length > 30
+    ) {
+      nameInput.classList.add('input--invalid');
+      return;
+    } else {
+      nameInput.classList.remove('input--invalid');
+    }
+
+    if (telInput.value.length != 18) {
+      telInput.classList.add('input--invalid');
+      return;
+    } else {
+      telInput.classList.remove('input--invalid');
+    }
+
+    if (!emailRegex.test(emailInput.value)) {
+      emailInput.classList.add('input--invalid');
+      return;
+    } else {
+      emailInput.classList.remove('input--invalid');
+    }
+    footerFormEl.submit();
+  };
+
+  const submitModalForm = (e) => {
+    e.preventDefault();
+    [modalNameInputEl, modalEmailInputEl, modalTelephoneInputEl].forEach(
+      (input) => {
+        if (!checkInputValidity(input)) {
+          input.classList.add('input--invalid');
+          return;
+        } else {
+          input.classList.remove('input--invalid');
+        }
+      }
+    );
+
+    if (
+      modalNameInputEl.value.length < 2 ||
+      modalNameInputEl.value.length > 30
+    ) {
+      modalNameInputEl.classList.add('input--invalid');
+      return;
+    } else {
+      modalNameInputEl.classList.remove('input--invalid');
+    }
+
+    if (modalTelephoneInputEl.value.length != 18) {
+      modalTelephoneInputEl.classList.add('input--invalid');
+      return;
+    } else {
+      modalTelephoneInputEl.classList.remove('input--invalid');
+    }
+
+    if (!emailRegex.test(modalEmailInputEl.value)) {
+      modalEmailInputEl.classList.add('input--invalid');
+      return;
+    } else {
+      modalEmailInputEl.classList.remove('input--invalid');
+    }
+    modalFormEl.submit();
+  };
+
+  const submitMiddleForm = (e) => {
+  e.preventDefault();
+    [middleNameInputEl, middleEmailInputEl, middleTelephoneInputEl].forEach(
+      (input) => {
+        if (!checkInputValidity(input)) {
+          input.classList.add('input--invalid');
+          return;
+        } else {
+          input.classList.remove('input--invalid');
+        }
+      }
+    );
+
+    if (
+      middleNameInputEl.value.length < 2 ||
+      middleNameInputEl.value.length > 30
+    ) {
+      middleNameInputEl.classList.add('input--invalid');
+      return;
+    } else {
+      middleNameInputEl.classList.remove('input--invalid');
+    }
+
+    if (middleTelephoneInputEl.value.length != 18) {
+      middleTelephoneInputEl.classList.add('input--invalid');
+      return;
+    } else {
+      middleTelephoneInputEl.classList.remove('input--invalid');
+    }
+
+    if (!emailRegex.test(middleEmailInputEl.value)) {
+      middleEmailInputEl.classList.add('input--invalid');
+      return;
+    } else {
+      middleEmailInputEl.classList.remove('input--invalid');
+    }
+    middleFormEl.submit();
+
+  }
+
+  document.querySelectorAll('.form-input-name').forEach((input) => {
+    input.addEventListener('input', (event) => {
+      const inputValue = event.target.value;
+      const regex = /^[a-zA-Zа-яА-Я]+$/;
+
+      if (!regex.test(inputValue)) {
+        event.target.value = inputValue.replace(/[^a-zA-Zа-яА-Я]+/g, '');
+      }
+      if (event.target.value.length < 2 || event.target.value.length > 30) {
+        input.classList.add('input--invalid');
+      } else {
+        input.classList.remove('input--invalid');
+      }
+    });
+  });
+
+  document.querySelectorAll('.form-input-tel').forEach((input) => {
+    input.addEventListener('input', () => {
+      if (input.value.length != 18) {
+        input.classList.add('input--invalid');
+      } else {
+        input.classList.remove('input--invalid');
+      }
+    });
+  });
+
+  document.querySelectorAll('.form-input-email').forEach((input) => {
+    input.addEventListener('input', (event) => {
+      const inputValue = event.target.value;
+      if (!emailRegex.test(inputValue)) {
+        input.classList.add('input--invalid');
+      } else {
+        input.classList.remove('input--invalid');
+      }
+    });
+  });
+
+  modalFormEl.addEventListener('submit', submitModalForm);
+  footerFormEl.addEventListener('submit', submitFooterForm);
+
+  if (middleFormEl) {
+    middleFormEl.addEventListener('submit', submitMiddleForm);
+  }
+});
